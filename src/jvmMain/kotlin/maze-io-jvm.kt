59b89e1f679030f526bee6567cda5831092e2eb1
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-06-02 15:13:06 +0200

    tune animated gif with history

--- src/jvmMain/kotlin/maze-io-jvm.kt
+++ src/jvmMain/kotlin/maze-io-jvm.kt
@@ -3,7 +3,6 @@ package org.jonnyzzz.lifegame
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_RGB
-import kotlin.math.max
 import kotlin.math.min
 
 fun Maze3.toImage(width: Int, height: Int): BufferedImage {
@@ -17,7 +16,7 @@ fun Maze3.toImage(width: Int, height: Int): BufferedImage {
   return img
 }
 
-fun Maze3.renderToImage(image: BufferedImage, color : Color = Color.BLACK): BufferedImage {
+fun Maze3.renderToImage(image: BufferedImage, color: Color = Color.BLACK): BufferedImage {
   val stepX = image.width / this.width
   val stepY = image.height / this.height
 
@@ -30,16 +29,18 @@ fun Maze3.renderToImage(image: BufferedImage, color : Color = Color.BLACK): Buff
 }
 
 fun BufferedImage.addAging() = apply {
-  for(x in 0 until width) {
-    for(y in 0 until height) {
+  for (x in 0 until width) {
+    for (y in 0 until height) {
       val p = Color(getRGB(x, y))
       if (p != Color.WHITE) {
-        setRGB(x,y, Color(
-          min(255, p.red * 5 / 3 + 33 ),
-          min(255, p.green * 5 / 3 + 33),
-          min(255, p.blue * 5 / 3 + 33)
-        ).rgb)
+        setRGB(
+          x, y, Color(
+            min(255, p.red * 5 / 8 + 133),
+            min(255, p.green * 5 / 8 + 133),
+            min(255, p.blue * 5 / 8 + 133)
+          ).rgb
+        )
       }
     }
-   }
+  }
 }
